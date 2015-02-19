f37414037d62afcb1f042ff6e766d8a6950c2ea0
Dhruv <dhruv.b.patel@hiof.no>
2015-02-19 08:38:42 +0100

    første commit

---
+++ hiofregister/students/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+	name = models.CharField(max_length=100)
+	email = models.EmailField()
+	study = models.CharField(max_length=255)
+
+	def __unicode__(self):
+		return '{},{}'.format(self.name, self.email)
+
+
